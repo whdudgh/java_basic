3ee8a0f88b7c69ff3c16662d2333a0223881694d
물렁복숭아 <dudgh114@gmail.com>
2023-07-31 16:15:28 +0900

    허브로갔나?

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
 		System.out.println("스테이징 커밋 한방에");
 		System.out.println("22222222222222222222");
 		System.out.println("33333333333333333333");
+		System.err.println("깃허브에 추가하는 테스트 문구");
 	}
 }
