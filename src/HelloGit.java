21fd1bcee37d5024b6b1a6d6c872b26dbb53cbde
물렁복숭아 <dudgh114@gmail.com>
2023-07-31 15:16:19 +0900

    이클립스 화면을 이용한 커밋 테스트

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("스테이징 커밋 한방에");
 		System.out.println("22222222222222222222");
+		System.out.println("33333333333333333333");
 	}
 }
